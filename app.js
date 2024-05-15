05910452c8a4a9cac23203f70c3d21e04f795546
revising <vipanshu.srivastava28@gmail.com>
2024-05-15 11:32:32 +0530

    added Draw condition

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 
 let turnO = true;
+let count = 0;
 
 const winPatterns = [
     [0,1,2],
@@ -19,6 +20,7 @@ const winPatterns = [
 
 const resetGame = () => {
     turnO = true;
+    count = 0;
     enableBoxes();
     msgContainer.classList.add("hide");
 };
@@ -34,9 +36,21 @@ boxes.forEach((box) => {
             turnO = true;
         }
         box.disabled = true;
-        checkWinner();
+        count++;
+
+        let isWinner = checkWinner();
+
+        if (count === 9 && !isWinner) {
+            gameDraw();
+        }
     });
 });
+
+const gameDraw = () => {
+    msg.innerText = `Game Draw🤝`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
 
 const disableBoxes = () => {
     for(let box of boxes) {
@@ -52,7 +66,7 @@ const enableBoxes = () => {
 }
 
 const showWinner = (winner) => {
-    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msg.innerText = `Congratulations! Winner is ${winner} 👏`;
     msgContainer.classList.remove("hide");
     disableBoxes();
 }
@@ -71,6 +85,7 @@ const checkWinner = () => {
             if (pos1Val === pos2Val && pos2Val === pos3Val) {
                 // console.log("Winnner", pos1Val);
                 showWinner(pos1Val);
+                return true;
             }
         }
     }
